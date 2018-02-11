ec4d670b24123e693f154b7e2585571e84d2032a
AmiBell <miratou96@hotmail.com>
2018-02-11 18:06:04 +0100

    first commit

---
+++ app/src/main/java/com/example/aspirev/myapplication/Selkni.java
@@ -0,0 +1,14 @@
+package com.example.aspirev.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Selkni extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selkni);
+    }
+}
+
